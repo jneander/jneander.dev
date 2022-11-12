1c598dbd98abe460aa2aac3c00cda07704544b85
Jeremy Neander <jeremy.neander@gmail.com>
2022-11-12 16:42:12 -0600

    perf: update font loading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,18 +12,16 @@ export default function Document() {
           content="Jeremy Neander is a software engineer working to build a better, more accessible web."
         />
 
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
         <link
-          href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,800italic,300,400,700,800"
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
           rel="stylesheet"
         />
 
         <link
-          href="https://fonts.googleapis.com/css?family=Lato:300italic,400italic,700italic,300,400,700"
-          rel="stylesheet"
-        />
-
-        <link
-          href="https://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,400italic,700,700italic"
+          href="https://fonts.googleapis.com/css2?family=Gentium+Book+Basic:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
 
