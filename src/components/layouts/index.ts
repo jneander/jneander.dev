5246b4864d009555d28b1d828f475340283cd56f
Jeremy Neander <jeremy.neander@gmail.com>
2022-11-13 13:05:10 -0600

    feat: revise styles on home page

--- src/components/layouts/index.ts
+++ src/components/layouts/index.ts
@@ -1,3 +1,4 @@
 export * from './default-layout'
 export * from './full-layout'
+export * from './layout-base'
 export * from './slim-layout'
--- src/components/pages/show-home/index.tsx
+++ src/components/pages/show-home/index.tsx
@@ -1,5 +1,5 @@
 import {LogoGithub, LogoLinkedin, LogoMastodon} from '../../icons'
-import {DefaultLayout} from '../../layouts'
+import {LayoutBase} from '../../layouts'
 import {Avatar} from './avatar'
 import {SocialLink} from './social-link'
 
@@ -7,46 +7,46 @@ import styles from './styles.module.scss'
 
 export function ShowHome() {
   return (
-    <DefaultLayout noName>
-      <div className="column one-third center">
-        <h1 className="visually-hidden">A Little About Jeremy Neander</h1>
+    <LayoutBase noName>
+      <main className={styles.Main}>
+        <div className={styles.Identity}>
+          <h1 className={styles.Name}>Jeremy Neander</h1>
 
-        <Avatar />
+          <Avatar />
 
-        <div className={styles.Name}>Jeremy Neander</div>
+          <div className={styles.SocialLinkList}>
+            <SocialLink SvgIcon={LogoGithub} href="https://github.com/jneander" name="GitHub" />
 
-        <div className={styles.SocialLinkList}>
-          <SocialLink SvgIcon={LogoGithub} href="https://github.com/jneander" name="GitHub" />
+            <SocialLink
+              SvgIcon={LogoMastodon}
+              href="https://mastodon.social/@jneander"
+              name="Mastodon"
+              rel="me"
+            />
 
-          <SocialLink
-            SvgIcon={LogoMastodon}
-            href="https://mastodon.social/@jneander"
-            name="Mastodon"
-            rel="me"
-          />
-
-          <SocialLink
-            SvgIcon={LogoLinkedin}
-            href="https://www.linkedin.com/in/jeremyneander"
-            name="Linkedin"
-          />
+            <SocialLink
+              SvgIcon={LogoLinkedin}
+              href="https://www.linkedin.com/in/jeremyneander"
+              name="Linkedin"
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="column two-thirds">
-        <p>
-          As a software craftsperson, I am continually developing my skills and practices to be of
-          the highest quality and greatest effect. Creative problem-solving, logic, pragmatism, and
-          a craving for knowledge drive me towards achieving both my personal and professional
-          goals.
-        </p>
+        <div>
+          <p>
+            As a software craftsperson, I am continually developing my skills and practices to be of
+            the highest quality and greatest effect. Creative problem-solving, logic, pragmatism,
+            and a craving for knowledge drive me towards achieving both my personal and professional
+            goals.
+          </p>
 
-        <p>
-          I am a life-long learner, constantly seeking new challenges and reaching outside my
-          comfort zone. When surrounded by passionate minds, the opportunities for growth are
-          boundless. Everyone has something to teach.
-        </p>
-      </div>
-    </DefaultLayout>
+          <p>
+            I am a life-long learner, constantly seeking new challenges and reaching outside my
+            comfort zone. When surrounded by passionate minds, the opportunities for growth are
+            boundless. Everyone has something to teach.
+          </p>
+        </div>
+      </main>
+    </LayoutBase>
   )
 }
