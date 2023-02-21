d1731e3796c28c45c3dfde1f0e955efa28e43549
Jeremy Neander <jeremy.neander@gmail.com>
2023-02-21 07:28:46 -0600

    feat: update constants

--- src/components/pages/show-resume/overview/constants.ts
+++ src/components/pages/show-resume/overview/constants.ts
@@ -1,5 +1,6 @@
 export const languageSkills = [
   'JavaScript',
+  'TypeScript',
   'Node.js',
   'Ruby',
   'Clojure',
@@ -9,8 +10,23 @@ export const languageSkills = [
   'Go',
 ]
 
-export const librarySkills = ['React', 'Webpack', 'InstUI', 'Redux', 'Rails', 'Sinatra', 'Android']
+export const librarySkills = [
+  'React',
+  'Next.js',
+  'Webpack',
+  'Storybook',
+  'Express',
+  'Rails',
+  'Sinatra',
+  'InstUI',
+]
 
 export const apiSkills = ['Goodreads', 'Canvas LMS', 'Amazon (Product Advertising, S3)']
 
-export const practiceSkills = ['BDD/TDD', 'Clean Code', 'Agile']
+export const practiceSkills = [
+  'BDD/TDD',
+  'Design Systems',
+  'Mentoring',
+  'Documentation',
+  'Estimations',
+]
